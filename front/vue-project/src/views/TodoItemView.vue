13511d1e9c9a228dae56551d63b9c897607312fa
spyker <121232224@qq.com>
2023-11-21 08:58:54 +0800

    update front

--- front/vue-project/src/views/TodoItemView.vue
+++ front/vue-project/src/views/TodoItemView.vue
@@ -3,42 +3,42 @@ import { ref } from 'vue'
 
 const newTodoText = ref('')
 const todos = ref([
-    {
-        id: 1,
-        title: 'Do the dishes'
-    },
-    {
-        id: 2,
-        title: 'Take out the trash'
-    },
-    {
-        id: 3,
-        title: 'Mow the lawn'
-    }
+  {
+    id: 1,
+    title: 'Do the dishes'
+  },
+  {
+    id: 2,
+    title: 'Take out the trash'
+  },
+  {
+    id: 3,
+    title: 'Mow the lawn'
+  }
 ])
 
 let nextTodoId = 4
 
 function addNewTodo() {
-    todos.value.push({
-        id: nextTodoId++,
-        title: newTodoText.value
-    })
-    newTodoText.value = ''
+  todos.value.push({
+    id: nextTodoId++,
+    title: newTodoText.value
+  })
+  newTodoText.value = ''
 }
 </script>
 
 <template>
-    <form v-on:submit.prevent="addNewTodo">
-        <label for="new-todo">Add a todo</label>
-        <input v-model="newTodoText" id="new-todo" placeholder="E.g. Feed the cat" />
-        <button>Add</button>
-    </form>
+  <form v-on:submit.prevent="addNewTodo">
+    <label for="new-todo">Add a todo</label>
+    <input v-model="newTodoText" id="new-todo" placeholder="E.g. Feed the cat" />
+    <button>Add</button>
+  </form>
 
-    <ul>
-        <li v-for="todo in todos" :key="todo.id" :title="todo.title">
-            {{ todo.title }}:{{ todo.id }}
-            <button @click="todos.splice(todo.id, 1)">Remove</button>
-        </li>
-    </ul>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id" :title="todo.title">
+      {{ todo.title }}:{{ todo.id }}
+      <button @click="todos.splice(todo.id, 1)">Remove</button>
+    </li>
+  </ul>
 </template>
