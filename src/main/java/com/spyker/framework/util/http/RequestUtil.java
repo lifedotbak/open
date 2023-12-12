203347ee0f4b0758269da367ff34cb91e2afe798
spyker <121232224@qq.com>
2023-12-12 09:26:59 +0800

    update

--- src/main/java/com/spyker/framework/util/http/RequestUtil.java
+++ src/main/java/com/spyker/framework/util/http/RequestUtil.java
@@ -1,98 +1,106 @@
 package com.spyker.framework.util.http;
 
-import com.spyker.framework.util.ExStringUtils;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.spyker.framework.util.ExStringUtils;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * Request工具类
  */
 public class RequestUtil extends HttpServlet {
 
-    public static HashMap<String, Object> getRequestParamAndHeader() {
+	/**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = 2492714953241153027L;
 
-        try {
-            HttpServletRequest request = getRequest();
-            if (request == null) {
-                return null;
-            }
+	public static HashMap<String, Object> getRequestParamAndHeader() {
 
-            //request信息
-            HashMap<String, Object> data = new HashMap<>();
-            HashMap<String, Object> requestParams = new HashMap<>();
-            Enumeration<String> paraNames = request.getParameterNames();
-            if (paraNames != null) {
-                for (Enumeration<String> enumeration = paraNames; enumeration.hasMoreElements(); ) {
-                    String key = enumeration.nextElement();
-                    requestParams.put(key, request.getParameter(key));
-                }
-            }
+		try {
+			HttpServletRequest request = getRequest();
+			if (request == null) {
+				return null;
+			}
 
-            HashMap<String, Object> requestFilter = new HashMap<>();
-            Enumeration<String> attributeNames = request.getAttributeNames();
-            if (attributeNames != null) {
-                for (Enumeration<String> attributeNames1 = attributeNames; attributeNames1.hasMoreElements(); ) {
-                    String key = attributeNames1.nextElement();
-                    if (key.contains("request_")) {
-                        requestFilter.put(key, request.getAttribute(key));
-                    }
-                }
-            }
+			// request信息
+			HashMap<String, Object> data = new HashMap<>();
+			HashMap<String, Object> requestParams = new HashMap<>();
+			Enumeration<String> paraNames = request.getParameterNames();
+			if (paraNames != null) {
+				for (Enumeration<String> enumeration = paraNames; enumeration.hasMoreElements();) {
+					String key = enumeration.nextElement();
+					requestParams.put(key, request.getParameter(key));
+				}
+			}
 
-            data.put("url", request.getRequestURL());
-            data.put("uri", request.getRequestURI());
-            data.put("method", request.getMethod());
-            data.put("request", requestParams);
-            data.put("request_filter", requestFilter);
+			HashMap<String, Object> requestFilter = new HashMap<>();
+			Enumeration<String> attributeNames = request.getAttributeNames();
+			if (attributeNames != null) {
+				for (Enumeration<String> attributeNames1 = attributeNames; attributeNames1.hasMoreElements();) {
+					String key = attributeNames1.nextElement();
+					if (key.contains("request_")) {
+						requestFilter.put(key, request.getAttribute(key));
+					}
+				}
+			}
 
-            //header 信息
-            Enumeration<String> headerNames = request.getHeaderNames();
-            HashMap<String, Object> headerParams = new HashMap<>();
-            if (headerNames != null) {
-                for (Enumeration<String> enumeration = headerNames; enumeration.hasMoreElements(); ) {
-                    String key = enumeration.nextElement();
-                    String value = request.getHeader(key);
-                    headerParams.put(key, value);
-                }
-            }
-            data.put("header", headerParams);
+			data.put("url", request.getRequestURL());
+			data.put("uri", request.getRequestURI());
+			data.put("method", request.getMethod());
+			data.put("request", requestParams);
+			data.put("request_filter", requestFilter);
 
-            return data;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+			// header 信息
+			Enumeration<String> headerNames = request.getHeaderNames();
+			HashMap<String, Object> headerParams = new HashMap<>();
+			if (headerNames != null) {
+				for (Enumeration<String> enumeration = headerNames; enumeration.hasMoreElements();) {
+					String key = enumeration.nextElement();
+					String value = request.getHeader(key);
+					headerParams.put(key, value);
+				}
+			}
+			data.put("header", headerParams);
 
-    public static HttpServletRequest getRequest() {
-        if (RequestContextHolder.getRequestAttributes() != null) {
-            return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
-        }
-        return null;
-    }
+			return data;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    public static String getDomain() {
-        HttpServletRequest request = getRequest();
-        if (request == null) {
-            return null;
-        }
-        return request.getServerName() + ":" + request.getServerPort();
-    }
+	public static HttpServletRequest getRequest() {
+		if (RequestContextHolder.getRequestAttributes() != null) {
+			return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
+					.getRequest();
+		}
+		return null;
+	}
 
-    public static String getUri(HttpServletRequest request) {
-        String uri = request.getRequestURI();
-        List<String> list = ExStringUtils.stringToArrayStrRegex(uri, "/");
-        list.removeIf(StringUtils::isNumeric); //去掉url中的数字参数
-        list.removeIf(c -> c.contains(","));// 去掉url中的逗号分隔参数
-        return StringUtils.join(list, "/");
-    }
+	public static String getDomain() {
+		HttpServletRequest request = getRequest();
+		if (request == null) {
+			return null;
+		}
+		return request.getServerName() + ":" + request.getServerPort();
+	}
+
+	public static String getUri(HttpServletRequest request) {
+		String uri = request.getRequestURI();
+		List<String> list = ExStringUtils.stringToArrayStrRegex(uri, "/");
+		list.removeIf(StringUtils::isNumeric); // 去掉url中的数字参数
+		list.removeIf(c -> c.contains(","));// 去掉url中的逗号分隔参数
+		return StringUtils.join(list, "/");
+	}
 
 }
