ba219ce98dc03fdbc0bf02f27f18c047015cf413
spyker <121232224@qq.com>
2023-12-29 08:21:43 +0800

    1.添加mybatis-plus插件

--- src/main/java/com/spyker/framework/mybatisplus/MybatisPlusConfig.java
+++ src/main/java/com/spyker/framework/mybatisplus/MybatisPlusConfig.java
@@ -1,6 +1,9 @@
 package com.spyker.framework.mybatisplus;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.DataChangeRecorderInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.IllegalSQLInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +13,32 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 public class MybatisPlusConfig {
 
-    @Bean
-    public MybatisPlusInterceptor paginationInterceptor() {
+	@Bean
+	public MybatisPlusInterceptor paginationInterceptor() {
 
-        /**
-         * 分页
-         */
-        MybatisPlusInterceptor mp = new MybatisPlusInterceptor();
-        mp.addInnerInterceptor(new PaginationInnerInterceptor());
-        return mp;
-    }
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+		/**
+		 * 分页插件 PaginationInnerInterceptor
+		 */
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+
+		/**
+		 * 非法SQL拦截插件 IllegalSQLInnerIntercepto
+		 */
+		interceptor.addInnerInterceptor(new IllegalSQLInnerInterceptor());
+
+		/**
+		 * 防全表更新与删除插件 BlockAttackInnerInterceptor
+		 */
+		interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+
+		/**
+		 * 数据变动记录插件
+		 * #DataChangeRecorderInnerInterceptor
+		 */
+		interceptor.addInnerInterceptor(new DataChangeRecorderInnerInterceptor());
+
+		return interceptor;
+	}
 }
