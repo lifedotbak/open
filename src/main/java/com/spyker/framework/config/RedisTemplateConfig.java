583ea004c5fcbf618520a9de361bfccce93131f8
spyker <121232224@qq.com>
2024-03-05 08:24:29 +0800

    1.redis repo

--- src/main/java/com/spyker/framework/config/RedisTemplateConfig.java
+++ src/main/java/com/spyker/framework/config/RedisTemplateConfig.java
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
+/**
+ * 通过与 Redis Repository 合作，你可以在 Redis Hashe 中无缝转换和存储domain对象，应用自定义映射策略，并使用二级索引 Redis Repository 至少需要
+ * Redis Server 2.8.0 版本，并且不能使用事务。请确保使用 禁用事务支持 的 RedisTemplate。
+ */
+@EnableRedisRepositories
 public class RedisTemplateConfig {
 
     @Bean
@@ -16,6 +22,8 @@ public class RedisTemplateConfig {
 
         redisTemplate.setKeySerializer(RedisSerializer.string());
         redisTemplate.setValueSerializer(RedisSerializer.json());
+
+        redisTemplate.setEnableTransactionSupport(false);
 
         return redisTemplate;
     }
