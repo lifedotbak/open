4a667b84bf5062fff0bd8527e255b1278e01da7c
spyker <121232224@qq.com>
2024-04-18 10:24:59 +0800

    onvif实现

---
+++ src/test/java/com/spyker/commons/onvif/OnvifUtilsTest.java
@@ -0,0 +1,45 @@
+package com.spyker.commons.onvif;
+
+import com.spyker.framework.onvif.OnvifUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class OnvifUtilsTest {
+
+    private static final String cameraAddress = "192.168.15.225";
+
+    //    @Test
+    void getToken() throws Exception {
+
+        //        String cameraAddress = "192.168.3.138";
+        String user = "admin";
+        String password = "123456";
+
+        OnvifUtils utils = new OnvifUtils(cameraAddress, user, password);
+
+        log.info(utils.getToken());
+    }
+
+    //    @Test
+    void getSnapshotUri() throws Exception {
+
+        String user = "admin";
+        String password = "123456";
+
+        OnvifUtils utils = new OnvifUtils(cameraAddress, user, password);
+
+        log.info(utils.getSnapshotUri("mainMediaProfileToken"));
+    }
+
+    //    @Test
+    void snapshot() throws Exception {
+
+        String user = "admin";
+        String password = "123456";
+
+        OnvifUtils utils = new OnvifUtils(cameraAddress, user, password);
+
+        utils.snapshot("mainMediaProfileToken", "d:/xxx/xxx/xx/56hghfh.jpg");
+    }
+}
